907b10e3a6865cd425090c7987614bd08492fadd
tiagobarre <46169817+tiagobarre@users.noreply.github.com>
2021-11-08 18:13:52 -0300

    Adicionar arquivos de projeto.

---
+++ CandidateTesting.TiagoSantosBarreto.ProjetoCDN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CandidateTesting.TiagoSantosBarreto.ProjetoCDN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
